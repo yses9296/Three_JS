e8e465e953624f4856d13a5797b32c94a04df7fa
Eunseo Choi <yses9296@gmail.com>
2023-02-02 20:23:45 +0900

    2023-02-02

---
+++ Tutorial/src/App.js
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+
+export default function App($app) {
+    const scene = new THREE.Scene();
+
+    console.log(scene)
+}
---
+++ Tutorial/src/main.js
@@ -0,0 +1,5 @@
+import * as THREE from '../node_modules/three/build/three.module';
+
+const scene = new THREE.Scene();
+
+console.log(scene)
